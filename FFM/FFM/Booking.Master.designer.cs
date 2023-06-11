e7116a9aabbb469f85f135ddc6dc6ddf5704d92a
huykd36 <huykd36@gmail.com>
2023-06-11 19:54:26 +0700

    sua loi

--- FFM/FFM/Booking.Master.designer.cs
+++ FFM/FFM/Booking.Master.designer.cs
@@ -31,13 +31,13 @@ namespace FFM {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Logo_FFM control.
+        /// logo_ffm control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton Logo_FFM;
+        protected global::System.Web.UI.WebControls.ImageButton logo_ffm;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
